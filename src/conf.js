15d9f56b4730e7a8a9f5bf3fd4290d62af594b23
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-10-05 22:30:33 +0700

    ok

--- src/conf.js
+++ src/conf.js
@@ -33,3 +33,38 @@ export const items = [
     value: 1000,
   },
 ];
+
+export const subs = [
+  {
+    sku: 'com.entipskta_sub_1',
+    value: 10,
+  },
+  {
+    sku: 'com.entipskta_sub_2',
+    value: 50,
+  },
+  {
+    sku: 'com.entipskta_sub_3',
+    value: 100,
+  },
+  {
+    sku: 'com.entipskta_sub_4',
+    value: 200,
+  },
+  {
+    sku: 'com.entipskta_sub_5',
+    value: 400,
+  },
+  {
+    sku: 'com.entipskta_sub_6',
+    value: 600,
+  },
+  {
+    sku: 'com.entipskta_sub_7',
+    value: 800,
+  },
+  {
+    sku: 'com.entipskta_sub_8',
+    value: 1000,
+  },
+];
