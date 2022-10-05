e685c7efa7450321cb7f1a4635a53e6f57c55fb1
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-10-05 21:52:30 +0700

    ok

--- src/conf.js
+++ src/conf.js
@@ -1,35 +1,70 @@
 /* eslint-disable prettier/prettier */
 export const items = [
   {
-    sku: 'com.solarsystem_1',
+    sku: 'com.solarsys_1',
     value: 10,
   },
   {
-    sku: 'com.solarsystem_2',
+    sku: 'com.solarsys_2',
     value: 50,
   },
   {
-    sku: 'com.solarsystem_3',
+    sku: 'com.solarsys_3',
     value: 100,
   },
   {
-    sku: 'com.solarsystem_4',
+    sku: 'com.solarsys_4',
     value: 200,
   },
   {
-    sku: 'com.solarsystem_5',
+    sku: 'com.solarsys_5',
     value: 400,
   },
   {
-    sku: 'com.solarsystem_6',
+    sku: 'com.solarsys_6',
     value: 600,
   },
   {
-    sku: 'com.solarsystem_7',
+    sku: 'com.solarsys_7',
     value: 800,
   },
   {
-    sku: 'com.solarsystem_8',
+    sku: 'com.solarsys_8',
+    value: 1000,
+  },
+];
+
+export const subs = [
+  {
+    sku: 'com.solarsys_sub_1',
+    value: 10,
+  },
+  {
+    sku: 'com.solarsys_sub_2',
+    value: 50,
+  },
+  {
+    sku: 'com.solarsys_sub_3',
+    value: 100,
+  },
+  {
+    sku: 'com.solarsys_sub_4',
+    value: 200,
+  },
+  {
+    sku: 'com.solarsys_sub_5',
+    value: 400,
+  },
+  {
+    sku: 'com.solarsys_sub_6',
+    value: 600,
+  },
+  {
+    sku: 'com.solarsys_sub_7',
+    value: 800,
+  },
+  {
+    sku: 'com.solarsys_sub_8',
     value: 1000,
   },
 ];
