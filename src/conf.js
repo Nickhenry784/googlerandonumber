b7e1e25784b53e564e80c56dc4e42302c537f4c4
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-10-05 22:04:01 +0700

    ok

--- src/conf.js
+++ src/conf.js
@@ -1,35 +1,70 @@
 /* eslint-disable prettier/prettier */
 export const items = [
   {
-    sku: 'com.essfood_1',
+    sku: 'com.awedface_1',
     value: 10,
   },
   {
-    sku: 'com.essfood_2',
+    sku: 'com.awedface_2',
     value: 50,
   },
   {
-    sku: 'com.essfood_3',
+    sku: 'com.awedface_3',
     value: 100,
   },
   {
-    sku: 'com.essfood_4',
+    sku: 'com.awedface_4',
     value: 200,
   },
   {
-    sku: 'com.essfood_5',
+    sku: 'com.awedface_5',
     value: 400,
   },
   {
-    sku: 'com.essfood_6',
+    sku: 'com.awedface_6',
     value: 600,
   },
   {
-    sku: 'com.essfood_7',
+    sku: 'com.awedface_7',
     value: 800,
   },
   {
-    sku: 'com.essfood_8',
+    sku: 'com.awedface_8',
+    value: 1000,
+  },
+];
+
+export const subs = [
+  {
+    sku: 'com.awedface_sub_1',
+    value: 10,
+  },
+  {
+    sku: 'com.awedface_sub_2',
+    value: 50,
+  },
+  {
+    sku: 'com.awedface_sub_3',
+    value: 100,
+  },
+  {
+    sku: 'com.awedface_sub_4',
+    value: 200,
+  },
+  {
+    sku: 'com.awedface_sub_5',
+    value: 400,
+  },
+  {
+    sku: 'com.awedface_sub_6',
+    value: 600,
+  },
+  {
+    sku: 'com.awedface_sub_7',
+    value: 800,
+  },
+  {
+    sku: 'com.awedface_sub_8',
     value: 1000,
   },
 ];
