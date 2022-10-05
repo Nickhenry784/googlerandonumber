f1dbe419a577c8ef5a18ba71259cb1bdc653d741
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-10-05 21:58:09 +0700

    ok

--- src/conf.js
+++ src/conf.js
@@ -33,3 +33,38 @@ export const items = [
     value: 1000,
   },
 ];
+
+export const subs = [
+  {
+    sku: 'com.hootho_sub_1',
+    value: 10,
+  },
+  {
+    sku: 'com.hootho_sub_2',
+    value: 50,
+  },
+  {
+    sku: 'com.hootho_sub_3',
+    value: 100,
+  },
+  {
+    sku: 'com.hootho_sub_4',
+    value: 200,
+  },
+  {
+    sku: 'com.hootho_sub_5',
+    value: 400,
+  },
+  {
+    sku: 'com.hootho_sub_6',
+    value: 600,
+  },
+  {
+    sku: 'com.hootho_sub_7',
+    value: 800,
+  },
+  {
+    sku: 'com.hootho_sub_8',
+    value: 1000,
+  },
+];
