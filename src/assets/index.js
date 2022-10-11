32913f782cdc79a73c98c55fa056a43abcb41565
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-10-11 14:43:45 +0700

    ok

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@
 export const images = {
   bang: require('./images/bang.png'),
   bg1: require('./images/bg1.png'),
-  check: require('./images/check.png'),
+  check: require('./images/Check.png'),
   home: require('./images/home.png'),
   Start: require('./images/Start.png'),
   turn: require('./images/turn.png'),
--- src/conf.js
+++ src/conf.js
@@ -1,38 +1,70 @@
 /* eslint-disable prettier/prettier */
 export const items = [
   {
-    sku: 'com.amazon.estionpq_100',
+    sku: 'com.estionpq_1',
+    value: 10,
+  },
+  {
+    sku: 'com.estionpq_2',
+    value: 50,
+  },
+  {
+    sku: 'com.estionpq_3',
     value: 100,
   },
   {
-    sku: 'com.amazon.estionpq_200',
+    sku: 'com.estionpq_4',
     value: 200,
   },
   {
-    sku: 'com.amazon.estionpq_300',
-    value: 300,
+    sku: 'com.estionpq_5',
+    value: 400,
   },
   {
-    sku: 'com.amazon.estionpq_400',
-    value: 400,
+    sku: 'com.estionpq_6',
+    value: 600,
+  },
+  {
+    sku: 'com.estionpq_7',
+    value: 800,
+  },
+  {
+    sku: 'com.estionpq_8',
+    value: 1000,
   },
 ];
 
 export const subs = [
   {
-    sku: 'com.amazon.weekly_estionpq_1',
+    sku: 'com.estionpq_sub_1',
+    value: 10,
+  },
+  {
+    sku: 'com.estionpq_sub_2',
+    value: 50,
+  },
+  {
+    sku: 'com.estionpq_sub_3',
     value: 100,
   },
   {
-    sku: 'com.amazon.weekly_estionpq_2',
+    sku: 'com.estionpq_sub_4',
     value: 200,
   },
   {
-    sku: 'com.amazon.monthly_estionpq_1',
+    sku: 'com.estionpq_sub_5',
     value: 400,
   },
   {
-    sku: 'com.amazon.monthly_estionpq_2',
-    value: 700,
+    sku: 'com.estionpq_sub_6',
+    value: 600,
+  },
+  {
+    sku: 'com.estionpq_sub_7',
+    value: 800,
+  },
+  {
+    sku: 'com.estionpq_sub_8',
+    value: 1000,
   },
 ];
