e516881672a1b01a2640c5388314937187377cad
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-10-11 14:48:15 +0700

    ok

--- src/conf.js
+++ src/conf.js
@@ -1,38 +1,70 @@
 /* eslint-disable prettier/prettier */
 export const items = [
   {
-    sku: 'com.amazon.dcardon_100',
+    sku: 'com.dcardon_1',
+    value: 10,
+  },
+  {
+    sku: 'com.dcardon_2',
+    value: 50,
+  },
+  {
+    sku: 'com.dcardon_3',
     value: 100,
   },
   {
-    sku: 'com.amazon.dcardon_200',
+    sku: 'com.dcardon_4',
     value: 200,
   },
   {
-    sku: 'com.amazon.dcardon_300',
-    value: 300,
+    sku: 'com.dcardon_5',
+    value: 400,
   },
   {
-    sku: 'com.amazon.dcardon_400',
-    value: 400,
+    sku: 'com.dcardon_6',
+    value: 600,
+  },
+  {
+    sku: 'com.dcardon_7',
+    value: 800,
+  },
+  {
+    sku: 'com.dcardon_8',
+    value: 1000,
   },
 ];
 
 export const subs = [
   {
-    sku: 'com.amazon.weekly_dcardon_1',
+    sku: 'com.dcardon_sub_1',
+    value: 10,
+  },
+  {
+    sku: 'com.dcardon_sub_2',
+    value: 50,
+  },
+  {
+    sku: 'com.dcardon_sub_3',
     value: 100,
   },
   {
-    sku: 'com.amazon.weekly_dcardon_2',
+    sku: 'com.dcardon_sub_4',
     value: 200,
   },
   {
-    sku: 'com.amazon.monthly_dcardon_1',
+    sku: 'com.dcardon_sub_5',
     value: 400,
   },
   {
-    sku: 'com.amazon.monthly_dcardon_2',
-    value: 700,
+    sku: 'com.dcardon_sub_6',
+    value: 600,
+  },
+  {
+    sku: 'com.dcardon_sub_7',
+    value: 800,
+  },
+  {
+    sku: 'com.dcardon_sub_8',
+    value: 1000,
   },
 ];
