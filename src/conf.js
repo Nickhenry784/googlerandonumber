e5ddeb5f3e7beb9d2b237bb7da01675784d50658
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-10-11 14:39:54 +0700

    ok

--- src/conf.js
+++ src/conf.js
@@ -1,38 +1,70 @@
 /* eslint-disable prettier/prettier */
 export const items = [
   {
-    sku: 'com.amazon.uessdogi_100',
+    sku: 'com.uessdogi_1',
+    value: 10,
+  },
+  {
+    sku: 'com.uessdogi_2',
+    value: 50,
+  },
+  {
+    sku: 'com.uessdogi_3',
     value: 100,
   },
   {
-    sku: 'com.amazon.uessdogi_200',
+    sku: 'com.uessdogi_4',
     value: 200,
   },
   {
-    sku: 'com.amazon.uessdogi_300',
-    value: 300,
+    sku: 'com.uessdogi_5',
+    value: 400,
   },
   {
-    sku: 'com.amazon.uessdogi_400',
-    value: 400,
+    sku: 'com.uessdogi_6',
+    value: 600,
+  },
+  {
+    sku: 'com.uessdogi_7',
+    value: 800,
+  },
+  {
+    sku: 'com.uessdogi_8',
+    value: 1000,
   },
 ];
 
 export const subs = [
   {
-    sku: 'com.amazon.weekly_uessdogi_1',
+    sku: 'com.uessdogi_sub_1',
+    value: 10,
+  },
+  {
+    sku: 'com.uessdogi_sub_2',
+    value: 50,
+  },
+  {
+    sku: 'com.uessdogi_sub_3',
     value: 100,
   },
   {
-    sku: 'com.amazon.weekly_uessdogi_2',
+    sku: 'com.uessdogi_sub_4',
     value: 200,
   },
   {
-    sku: 'com.amazon.monthly_uessdogi_1',
+    sku: 'com.uessdogi_sub_5',
     value: 400,
   },
   {
-    sku: 'com.amazon.monthly_uessdogi_2',
-    value: 700,
+    sku: 'com.uessdogi_sub_6',
+    value: 600,
+  },
+  {
+    sku: 'com.uessdogi_sub_7',
+    value: 800,
+  },
+  {
+    sku: 'com.uessdogi_sub_8',
+    value: 1000,
   },
 ];
